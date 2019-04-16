839a3d23972a45cbbcbda0c01ba81f11363ed757
Andi Meier <andreas.meier4@students.fhnw.ch>
2019-04-16 16:19:21 +0200

    Slider player

--- src/birds.js
+++ src/birds.js
@@ -19,18 +19,18 @@ svg.append('rect')
 
 let g = svg.append('g');
 
-// let projection = d3.geoEquirectangular()
-//     .scale(300)
-//     .center([-50, 30]);
 let projection = d3.geoEquirectangular()
-    .scale(16000)
-    .center([-74.5, 40.5]);
+    .scale(300)
+    .center([-50, 30]);
+// let projection = d3.geoEquirectangular()
+//     .scale(16000)
+//     .center([-74.5, 40.5]);
 let path = d3.geoPath().projection(projection);
 
 
 Promise.all([
     d3.json('data/maps/world-50m.v1.json'),
-    d3.csv('data/birds/irma.csv')
+    d3.csv('data/birds/leo.csv')
 ])
 .then((data, error) => {
     if (error) console.log(error);
@@ -40,16 +40,16 @@ Promise.all([
 
     drawMap(map);
     let timeSlider = drawSlider(bird);
-    d3.select('#sliderValue').text(d3.timeFormat('%d.%m.%Y')(timeSlider.value()));
     let currentDate = d3.timeFormat('%Y-%m-%d')(timeSlider.value());
     drawBird(bird, currentDate);
 
     timeSlider.on('onchange', val => {
-        d3.select('#sliderValue').text(d3.timeFormat('%d.%m.%Y')(val));
         let currentDate = d3.timeFormat('%Y-%m-%d')(val);
         moveBird(bird, currentDate);
     });
 
+    const playButton = document.querySelector('#play');
+    playButton.onclick = () => play(bird, timeSlider);
 });
 
 
@@ -61,13 +61,18 @@ function drawMap(map) {
 
 function drawSlider(bird) {
     let timeData = bird.map(d => new Date(d.timestamp));
+    const year = '2005';
 
     let timeSlider = d3.sliderBottom()
+        // .min(timeData.find(d => d.getFullYear().toString() === year))
+        // .max(new Date(year + '-12-31'))
         .min(d3.min(timeData))
         .max(d3.max(timeData))
         .step(1000 * 60 * 60 * 24)
         .width(650)
-        .tickFormat(d3.timeFormat('%Y'));
+        .ticks(12)
+        .tickFormat(d3.timeFormat('%Y'))
+        .displayFormat(d3.timeFormat('%d.%m.%Y'));
 
     let gSlider = d3.select('#sliderTime')
         .append('svg')
@@ -107,3 +112,24 @@ function getPosition(d) {
     let pos = [d['location-long'], d['location-lat']];
     return projection(pos);
 }
+
+function play(bird, timeSlider) {
+    let start = null;
+
+    function step(timestamp) {
+        if (! start) {
+            start = timestamp;
+        }
+
+        let progress = timestamp - start;
+        let i = Math.round(progress / 10);
+        timeSlider.value(new Date(bird[i].timestamp));
+
+        if (i < bird.length - 1) {
+            requestAnimationFrame(step);
+        }
+    }
+
+    requestAnimationFrame(step);
+}
+
