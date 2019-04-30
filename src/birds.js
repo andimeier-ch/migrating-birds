caaa9223461e7d6d7ef66471173204d33cbb9e72
Andi Meier <andreas.meier4@students.fhnw.ch>
2019-04-30 13:17:53 +0200

    slider play/pause and selecting bird

--- src/birds.js
+++ src/birds.js
@@ -4,12 +4,12 @@ let svgWidth = 700;
 let svgHeight = 600;
 
 
-let svg = d3.select('#map')
+let map = d3.select('#map')
     .append('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight);
 
-svg.append('rect')
+map.append('rect')
     .attr('width', svgWidth)
     .attr('height', svgHeight)
     .style('stroke-width', 1)
@@ -17,7 +17,7 @@ svg.append('rect')
     .style('fill', 'white');
 
 
-let g = svg.append('g');
+let gMap = map.append('g');
 
 let projection = d3.geoEquirectangular()
     .scale(300)
@@ -28,44 +28,53 @@ let projection = d3.geoEquirectangular()
 let path = d3.geoPath().projection(projection);
 
 
-Promise.all([
-    d3.json('data/maps/world-50m.v1.json'),
-    d3.csv('data/birds/leo.csv')
-])
-.then((data, error) => {
-    if (error) console.log(error);
+let birdSelect = document.querySelector('#bird-select');
+birdSelect.onchange = event => {
+    loadData(birdSelect.value)
+};
 
-    let map = data[0];
-    let bird = data[1];
+loadData(birdSelect.value);
 
-    drawMap(map);
-    let timeSlider = drawSlider(bird);
-    let currentDate = d3.timeFormat('%Y-%m-%d')(timeSlider.value());
-    drawBird(bird, currentDate);
+function loadData(file) {
+    Promise.all([
+        d3.json('data/maps/world-50m.v1.json'),
+        d3.csv(`data/birds/${file}.csv`)
+    ])
+    .then((data, error) => {
+        if (error) console.log(error);
 
-    timeSlider.on('onchange', val => {
-        let currentDate = d3.timeFormat('%Y-%m-%d')(val);
-        moveBird(bird, currentDate);
+        let map = data[0];
+        let bird = data[1];
+
+        drawMap(map);
+        let timeSlider = drawSlider(bird);
+        let currentDate = d3.timeFormat('%Y-%m-%d')(timeSlider.value());
+        drawBird(bird, currentDate);
+
+        timeSlider.on('onchange', val => {
+            let currentDate = d3.timeFormat('%Y-%m-%d')(val);
+            moveBird(bird, currentDate);
+        });
+
+        const playButton = document.querySelector('#play');
+        const player = BirdPlayer(playButton, bird, timeSlider);
+        // playButton.onclick = event => player.togglePlay(bird, timeSlider);
+
     });
-
-    const playButton = document.querySelector('#play');
-    playButton.onclick = () => play(bird, timeSlider);
-});
-
+}
 
 function drawMap(map) {
-    g.append('path')
+    gMap.append('path')
         .datum(topojson.feature(map, map.objects.countries))
         .attr('d', path);
 }
 
 function drawSlider(bird) {
+    d3.selectAll('#sliderTime svg').remove();
+
     let timeData = bird.map(d => new Date(d.timestamp));
-    const year = '2005';
 
     let timeSlider = d3.sliderBottom()
-        // .min(timeData.find(d => d.getFullYear().toString() === year))
-        // .max(new Date(year + '-12-31'))
         .min(d3.min(timeData))
         .max(d3.max(timeData))
         .step(1000 * 60 * 60 * 24)
@@ -74,7 +83,7 @@ function drawSlider(bird) {
         .tickFormat(d3.timeFormat('%Y'))
         .displayFormat(d3.timeFormat('%d.%m.%Y'));
 
-    let gSlider = d3.select('#sliderTime')
+    let gSlider = d3.selectAll('#sliderTime')
         .append('svg')
         .attr('width', 700)
         .attr('height', 100)
@@ -86,8 +95,9 @@ function drawSlider(bird) {
 }
 
 function drawBird(bird, currentDate) {
-    g.selectAll('.bird-position').remove();
-    g.append('circle')
+    gMap.selectAll('.bird-position').remove();
+
+    gMap.append('circle')
         .datum(bird.find(b => b.timestamp === currentDate))
         .attr('class', 'bird-position')
         .attr('cx', d => getPosition(d)[0])
@@ -101,7 +111,7 @@ function moveBird(bird, currentDate) {
         .duration(100)
         .ease(d3.easeLinear);
 
-    g.select('.bird-position')
+    gMap.select('.bird-position')
         .datum(bird.find(b => b.timestamp === currentDate))
         .transition(transition)
         .attr('cx', d => getPosition(d)[0])
@@ -113,23 +123,68 @@ function getPosition(d) {
     return projection(pos);
 }
 
-function play(bird, timeSlider) {
-    let start = null;
+// function togglePlay(bird, timeSlider, event) {
+//     isPlaying = !isPlaying;
+//
+//     if (isPlaying) {
+//         play(bird, timeSlider);
+//         event.target.textContent = 'Pause';
+//     } else {
+//         event.target.textContent = 'Play';
+//     }
+// }
 
-    function step(timestamp) {
-        if (! start) {
-            start = timestamp;
+// function play(bird, timeSlider) {
+//     let start = null;
+//
+//     function step(timestamp) {
+//         if (!animationStart) {
+//             animationStart = timestamp;
+//         }
+//
+//         let progress = timestamp - animationStart;
+//         let i = Math.round(progress / 10);
+//         timeSlider.value(new Date(bird[i].timestamp));
+//
+//         if (i < bird.length - 1) {
+//             requestAnimationFrame(step);
+//         }
+//     }
+//
+//     requestAnimationFrame(step);
+// }
+
+
+const BirdPlayer = (button, bird, timeSlider) => {
+    let isPlaying = false;
+    let animationStart = null;
+
+    button.onclick = () => togglePlay();
+
+    const togglePlay = () => {
+        isPlaying = !isPlaying;
+
+        if (isPlaying) {
+            requestAnimationFrame(play);
+            button.textContent = 'Pause';
+        } else {
+            button.textContent = 'Play';
+        }
+    };
+
+    const play = (timestamp) => {
+        if (!animationStart) {
+            animationStart = timestamp;
         }
 
-        let progress = timestamp - start;
+        let progress = timestamp - animationStart;
         let i = Math.round(progress / 10);
         timeSlider.value(new Date(bird[i].timestamp));
 
-        if (i < bird.length - 1) {
-            requestAnimationFrame(step);
+        if (i < bird.length - 1 && isPlaying) {
+            requestAnimationFrame(play);
         }
-    }
+    };
 
-    requestAnimationFrame(step);
-}
 
+};
