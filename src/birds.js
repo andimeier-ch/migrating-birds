80ca804cce05b64841669d1d047482b91d48fad8
My-Nhien Nguyen <mynhien.nguyen@students.fhnw.ch>
2019-05-18 11:24:30 +0200

    fixed data

--- src/birds.js
+++ src/birds.js
@@ -10,8 +10,6 @@ birdSelect.onchange = event => {
 // create map
 const map = L.map('map')
     .setView([25, 30], 2.5)
-    .setMaxZoom(3)
-    .setMinZoom(3);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
